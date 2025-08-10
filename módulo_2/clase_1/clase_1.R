b6f0dd48827d175f1e24fd2658b0414531521f46
bastianolea <bastianolea@gmail.com>
2025-08-10 15:08:45 -0400

    código clases 1 y 2

--- módulo_2/clase_1/clase_1.R
+++ módulo_2/clase_1/clase_1.R
@@ -1,29 +1,27 @@
-# read.csv2()
-
-library(readr)
-# read_csv2() # carga en una tabla tibble
+# Cargar datos desde archivos Excel
 
 # instalar el paquete
-
-# install.packages("readxl")
+install.packages("readxl")
 # install.packages("openxlsx")
 
 library(readxl)
 
-# readxl::read_xlsx()
-
+# Cargar los datos
 datos <- read_xlsx("datos/campamentos_chile_2024.xlsx")
 
 datos
 
+# Explorar los datos
 library(dplyr)
 
+# Seleccionar columnas
 datos |> 
   select(nombre, region)
 
 datos |> 
   select(hogares, hectareas)
 
+# Seleccionar por posición
 datos |> 
   select(1, 2, 3, 4, 8, 9)
 
@@ -33,15 +31,19 @@ datos |>
 datos |> 
   select(1:10)
 
+# seleccionar las que contienen parcialmente un texto
 datos |> 
   select(contains("hect"))
 
+# seleccionar las que empiecen con un texto
 datos |> 
   select(starts_with("cut"))
 
+# seleccionar las que no empiezan con un texto
 datos |> 
   select(-starts_with("cut"))
 
+# seleccionar las que son de un cierto tipo
 datos |> 
   select(nombre, where(is.numeric))
 
@@ -52,52 +54,62 @@ datos |>
   select(nombre, where(is.numeric)) |> 
   select(-cut)
 
+# seleccionar las que terminan con un texto
 datos |> 
   select(ends_with("_p"))
 
 
-
-# conteos
+# conteos ----
+# conteo de valores únicos de una variable
 datos |> 
   count(region)
 
+# contar y ordenar
 datos |> 
   count(region) |> 
   arrange(n)
 
+# contar y ordenar descendentemente
 datos |> 
   count(region) |> 
   arrange(desc(n))
 
+# contar y ordenar dentro de la misma función de conteo
 datos |> 
   count(provincia, sort = TRUE)
 
+# contar, ordenar, y renombrar
 datos |> 
   count(provincia) |> 
   arrange(desc(n)) |> 
   rename(conteo = n)
 
+# las tres operaciones dentro de la misma función
 datos |> 
   count(provincia, 
         sort = TRUE, 
         name = "conteo")
 
+# guardar el conteo como un nuevo objeto
 datos_conteo <- datos |> 
   count(provincia, 
         sort = TRUE, 
         name = "conteo")
 
-# guardarlo como un excel
-library(writexl)
-# install.packages("writexl")
-
 datos_conteo
 
+# guardarlo como un excel
+install.packages("writexl")
+library(writexl)
+
+# guardar el archivo dentro de una carpeta
 write_xlsx(datos_conteo, "datos/conteo.xlsx")
 
+# lo mismo pero usando un conector
 datos_conteo |> 
   write_xlsx("datos/conteo.xlsx")
 
+# lo mismo, de principio a fin, desde el dato original hasta guardarlo en Excel
 datos |> 
   count(provincia, 
         sort = TRUE, 
@@ -105,7 +117,7 @@ datos |>
   filter(conteo > 50) |> 
   write_xlsx("datos/conteo.xlsx")
 
-
+# incluso se puede hacer lo mismo desde la carga del archivo
 read_xlsx("datos/campamentos_chile_2024.xlsx") |> 
   count(provincia, 
         sort = TRUE, 
@@ -113,11 +125,16 @@ read_xlsx("datos/campamentos_chile_2024.xlsx") |>
   filter(conteo > 50) |> 
   write_xlsx("datos/conteo.xlsx")
 
+# filtrar datos ----
+
+# filtrar observaciones que son mayores a
 datos |> 
   filter(hogares > 100)
 
+# filtrar las observaciones que no son mayores a
 datos |> 
   filter(!hogares > 100)
+# el ! es el operador de negación, que invierte cualquier comparación
 
 datos |> 
   filter(hogares <= 100)
@@ -131,34 +148,40 @@ datos |>
 datos |> 
   filter(nombre == "Bellavista")
 
+# filtrar los elementos que están dentro de un conjunto
 datos |> 
   filter(nombre %in% c("Bellavista", "Los Fleteros", "Manuel Rodríguez"))
 
+# invertirlo anterior: elementos que no están dentro de un conjunto
 datos |> 
   filter(!nombre %in% c("Bellavista", "Los Fleteros", "Manuel Rodríguez"))
 
-
-
+# crear un vector con los valores
 lista_campamentos <- c("Bellavista", "Los Fleteros", "Manuel Rodríguez", "Manzana 33")
 
+# filtrar observaciones que están dentro del vector con los valores
 datos |> 
   filter(nombre %in% lista_campamentos)
 
+# por dentro, está ocurriendo esto
 datos$nombre %in% lista_campamentos
+# se genera un vector de verdaderos y falsos, con los que se filtran los datos
 
+
+# crear variables ----
 datos |> 
   select(1:4) |> 
   mutate(variable = 1)
 
+# Crear una variable que contenga el resultado de una comparación
 datos |> 
   select(1:4) |> 
   mutate(seleccion = nombre %in% lista_campamentos)
+# La variable estará llena de verdaderos y falsos
 
 datos |> 
   select(1:4) |> 
   mutate(seleccion = nombre %in% c("Bellavista", "Los Fleteros", "Manuel Rodríguez", "Manzana 33"))
-
-
 
 datos |> 
   select(1:4, hogares) |> 
@@ -168,28 +191,26 @@ datos |>
   select(1:4, hogares) |> 
   mutate(grandes = hogares > 60)
 
+# Crear una variable con una función condicional, que rellenará con los valores entregados dependiendo de si la observación cumple con la condición
 datos |> 
   select(1:4, hogares) |> 
   mutate(grandes = ifelse(hogares > 60, 
                           "sí", "no"))
 
+# Crear tres variables en base de condicionales sobre una columna
 datos |> 
   select(1:4, hogares) |> 
   mutate(grandes = ifelse(hogares > 60, "sí", "no")) |> 
   mutate(medianos = ifelse(hogares > 30, "sí", "no")) |> 
   mutate(chicos = ifelse(hogares > 10, "sí", "no"))
 
+# Hacer un filtro después de haber creado una variable con condicional
 datos |> 
   select(1:4, hogares) |> 
   mutate(grandes = ifelse(hogares > 60, "grandes", "no")) |> 
   mutate(medianos = ifelse(hogares > 30, "medianos", "no")) |> 
   mutate(chicos = ifelse(hogares > 10, "chicos", "no")) |> 
   filter(medianos == "medianos")
-
-datos |> 
-  mutate(a = 1,
-         b = 2,
-         c = 3)
 
 datos |> 
   select(1:4, hogares) |> 
@@ -205,7 +226,7 @@ datos |>
   filter(hogares > 100,
          hectareas > 4)
 
-
+# Crear un subconjunto de los datos a partir de un filtro
 datos_2 <- datos |> 
   select(nombre, region, comuna, hogares) |> 
   filter(region != "Valparaíso")
@@ -213,18 +234,41 @@ datos_2 <- datos |>
 datos_2 |> 
   mutate(grupo = ifelse(hogares > 40, "grupo a", "grupo b"))
 
+# crea una variable más compleja utilizando la función case_when()
+# esta función opera como varios ifelse() en serie, permitiendo crear 
+# una nueva variable que contenga distintos valores dependiendo de 
+# las condiciones que se cumplen
 datos_2 |> 
   mutate(tamaños = case_when(hogares > 40 ~ "grandes",
                              hogares <= 40 & hogares > 30 ~ "medianos",
                              hogares <= 30 ~ "pequeños"))
 
+# guardar el resultado como un objeto
 datos_conteo_2 <- datos_2 |> 
   mutate(tamaños = case_when(hogares > 80 ~ "muy grandes",
                              hogares > 40 & hogares <= 80 ~ "grandes",
                              hogares <= 40 & hogares > 20 ~ "medianos",
                              hogares <= 20 ~ "pequeños"))
 
+datos_conteo_2
+
+# ver el resultado de la variable que creamos
 datos_conteo_2 |> count(tamaños)
+
+# También se puede ir creando la decodificación por pasos, dado que 
+# las observaciones que adquieren un resultado
+# no vuelven a ser sobrescritas por las siguientes condiciones
+datos_2 |> 
+  mutate(tamaños = case_when(hogares > 80 ~ "muy grandes"))
+
+datos_2 |> 
+  mutate(tamaños = case_when(hogares > 80 ~ "muy grandes",
+                             hogares > 40 ~ "grandes"))
+
+datos_2 |> 
+  mutate(tamaños = case_when(hogares > 80 ~ "muy grandes",
+                             hogares > 40 ~ "grandes",
+                             hogares > 30 ~ "medianos"))
 
 datos_2 |> 
   mutate(tamaños = case_when(hogares > 80 ~ "muy grandes",
@@ -232,13 +276,22 @@ datos_2 |>
                              hogares > 30 ~ "medianos",
                              hogares <= 30 ~ "pequeños")) #|> count(tamaños)
 
+# resúmenes ----
+# ¿qué pasa si queremos saber algo sobre una columna completa, como su valor mayor, su promedio, etc.?
 datos_2 |> arrange(hogares)
 min(datos_2$hogares)
 max(datos_2$hogares)
 
+# Podemos crear una columna que contenga una operación que se aplica a la columna entera
 datos_2 |> 
   mutate(min_hogares = min(hogares),
          max_hogares = max(hogares))
+# Pero el resultado aparece repetido por cada observación, lo que puede ser confuso
+
+# usamos la función summarize() para obtener resúmenes de un dataframe 
+# a partir de operaciones que se realizan sobre sus columnas
+datos_2 |> 
+  summarise(maximo = max(hogares))
 
 datos_2 |> 
   summarise(minimo = min(hogares),
@@ -257,6 +310,7 @@ datos |>
             percentil_75 = quantile(hogares, .75),
             maximo = max(hogares))
 
+# lo mismo aplicado a una variable distinta
 datos |> 
   summarise(minimo = min(hectareas),
             percentil_25 = quantile(hectareas, .25),
@@ -265,6 +319,9 @@ datos |>
             percentil_75 = quantile(hectareas, .75),
             maximo = max(hectareas))
 
+# truco para reusar este tipo de expresiones en varias columnas: 
+# renombrar la columna primero, para que si quieres aplicarlo a otra columna
+# solo necesitas cambiar el nombre en el rename()
 datos |> 
   rename(variable = hogares) |> 
   summarise(minimo = min(variable),
@@ -274,6 +331,7 @@ datos |>
             percentil_75 = quantile(variable, .75),
             maximo = max(variable))
 
+# crear una función que hace lo mismo
 estadisticos <- function(x) {
   x |> 
     summarise(minimo = min(variable),
@@ -284,12 +342,15 @@ estadisticos <- function(x) {
               maximo = max(variable)) 
 }
 
+# usar la función creada
 datos |> rename(variable = hogares) |> estadisticos()
 datos |> rename(variable = hectareas) |> estadisticos()
 datos |> rename(variable = area) |> estadisticos()
 
 
-
+# operaciones agrupadas ----
+# Cualquier operación, ya sea un resumen o la creación de una variable nueva, 
+# puede ser realizada en separado por grupos de una variable anteponiento group_by()
 datos |> 
   group_by(region) |> 
   summarise(minimo = min(hogares),
@@ -298,13 +359,7 @@ datos |>
             mediana = median(hogares),
             percentil_75 = quantile(hogares, .75),
             maximo = max(hogares))
-
-
-datos_2 |> 
-  mutate(tamaños = case_when(hogares > 80 ~ "muy grandes",
-                             hogares > 40 ~ "grandes",
-                             hogares > 30 ~ "medianos",
-                             hogares <= 30 ~ "pequeños"))
+# En este caso, los resúmenes estadísticos se calculan por los grupos que da la variable región
 
 datos_2 |> 
   mutate(min_hogares = min(hogares),
@@ -339,10 +394,12 @@ datos_2 |>
             max_hogares = max(hogares),
             prom_hogares = mean(hogares))
 
+# Contar la cantidad de observaciones en cada grupo obteniendo un resumen agrupados y usando la función n()
 datos_2 |> 
   group_by(region) |> 
   summarise(n())
 
+# se obtiene el mismo resultado que haber aplicado count()
 datos_2 |> 
   count(region)
 
@@ -369,6 +426,7 @@ datos_2 |>
             max_hogares = max(hogares),
             prom_hogares = mean(hogares))
 
+# función para recodificar variables continuas cortándolas en distintos valores
 datos |> 
   select(1:2, comuna, hectareas) |> 
   mutate(grupo = cut(hectareas, c(0, 1, 3, 5)))
@@ -383,7 +441,8 @@ datos |>
 datos |> 
   mutate(prueba = hogares/hectareas)
 
-
+# recodificación ----
+# Reemplazar los valores de una variable por otros en base a coincidencias exactas
 datos |> 
   select(1:5) |> 
   # distinct(region) |>
--- módulo_2/clase_2/clase_2.R
+++ módulo_2/clase_2/clase_2.R
@@ -1,55 +1,56 @@
+# cargar paquetes
 library(dplyr)
 library(readxl)
+# recordemos que tienen que haber sido previamente instalados para poder cargarlos
 
 pobreza <- read_xlsx("datos/pobreza_comunas_2022.xlsx")
 
-pobreza |> 
-  slice(-1)
+# si exploramos el dataframe cargado, vemos que se carga de forma incorrecta
+pobreza
+# los nombres de la columna están en una de las filas del dataframe!
 
+# con slice podemos eliminar una fila del dataframe, pero no es suficiente
+pobreza |> slice(-1)
+
+# instalar paquete {janitor}, que contiene funciones que nos ayudan a arreglar esto
 install.packages("janitor")
 library(janitor)
 
-pobreza |> 
+# con janitor le decimos que una de las filas del dataframe se transforme en los nombres de las columnas
+pobreza <- pobreza |> 
   janitor::row_to_names(2)
-
-
-pobreza <- read_xlsx("datos/pobreza_comunas_2022.xlsx", skip = 2)
 
 pobreza
 
+# otra opción sería usar el argumento skip de read_xlsx para que se salte n cantidad de filas
+read_xlsx("datos/pobreza_comunas_2022.xlsx", skip = 2)
+
+# explorar los datos
 glimpse(pobreza)
 
+# seleccionar dos columnas
 pobreza |> 
   select(Región, `Número de personas según proyecciones de población (*)`)
+# los nombres de columna son muy largos!
 
+# renombrar columnas
 pobreza |> 
   rename(region = Región,
          poblacion = `Número de personas según proyecciones de población (*)`,
          pobreza_multi = `Número de personas en situación de pobreza multidimensional (**)`)
 
+# renombrar columnas por su posición
 pobreza |> 
   rename(region = 2,
          poblacion = 4,
          pobreza_multi = 5)
 
+# Limpiar los nombres de las columnas
 pobreza |> 
   janitor::clean_names()
 
+# Mezclada la limpieza de nombres de columnas con renombrar algunas otras que siguen siendo largas
 pobreza_2 <- pobreza |> 
-  janitor::clean_names() |>
-  rename(poblacion = 4,
-         pobreza_personas = 5,
-         pobreza_porcentaje = 6)
-
-names(pobreza_2)
-
-pobreza_2
-
-
-pobreza_malo <- read_xlsx("datos/pobreza_comunas_2022.xlsx")
-
-pobreza_malo_2 <- pobreza_malo |> 
-  row_to_names(2) |> 
   janitor::clean_names() |>
   rename(poblacion = 4,
          pobreza_personas = 5,
@@ -57,74 +58,102 @@ pobreza_malo_2 <- pobreza_malo |>
          casen = presencia_de_la_comuna_en_la_muestra_casen,
          tipo = tipo_de_estimacion_sae)
 
-pobreza_malo_2 |> 
+# ver los nuevos nombres de las columnas
+names(pobreza_2)
+
+pobreza_2
+
+pobreza_2 |> 
   print(n = 100)
 
-pobreza_malo_2 |> 
-  mutate(pobreza_personas = as.numeric(pobreza_personas)) |> 
-  summarize(sum(pobreza_personas))
+# revisar el tipo de una variable
+class(pobreza_2$pobreza_personas)
 
-pobreza_malo_2 |> 
+# sumar todos los valores de una variable
+pobreza_2 |> 
+  # convertir a numérico primero
+  mutate(pobreza_personas = as.numeric(pobreza_personas)) |> 
+  # sumar por medio de la función de resumen
+  summarize(sum(pobreza_personas))
+# la suma no funciona porque hay datos periddos en esta variable
+
+# filtrar datos perdidos en una variable
+pobreza_2 |> 
+  filter(is.na(pobreza_personas))
+
+# encontrar el problema
+pobreza_2 |> tail() # ver el final de los datos
+
+pobreza_2 |> 
   filter(is.na(pobreza_personas)) |> 
   select(codigo)
 
-
-pobreza_malo_3 <- pobreza_malo_2 |> 
+# eliminar los datos perdidos de esa variable
+pobreza_3 <- pobreza_2 |> 
   filter(!is.na(pobreza_personas))
 
-pobreza_malo_2 |> tail()
-pobreza_malo_3 |> tail()
+# confirmar el final de los datos
+pobreza_3 |> tail()
 
-pobreza_malo_3 |> 
+# obtener varios resumenes estadísticos de la variable limpiada
+pobreza_3 |> 
   mutate(pobreza_personas = as.numeric(pobreza_personas)) |> 
   summarize(sum(pobreza_personas),
             max(pobreza_personas),
             mean(pobreza_personas))
 
-
-pobreza_malo_2 |> 
-  filter(is.na(pobreza_porcentaje))
-
-
+# paquete para ensuciar datos
+install.packages("messy")
 library(messy)
 
-pobreza_malo_3b <- pobreza_malo_3 |> 
+# agregar datos perdidos a la variable, para probar limpieza de datos
+pobreza_3b <- pobreza_3 |> 
   make_missing(cols = "pobreza_personas")
 
-pobreza_malo_3b |> 
+# ahora hay más datos perdidos en la variable
+pobreza_3b |> 
   filter(is.na(pobreza_personas))
 
-pobreza_malo_3b |> 
+# contar datos perdidos en una o más columnas
+pobreza_3b |> 
   summarize(sum(is.na(pobreza_personas)),
             sum(is.na(pobreza_porcentaje)))
 
-
-pobreza_malo_3b |> 
+# podemos ignorar los dados perdidos con el argumento na.rm = TRUE
+pobreza_3b |> 
+  # convertir a numérico
   mutate(pobreza_personas = as.numeric(pobreza_personas)) |> 
+  # contar missing y además hacer la suma ignorando missings
   summarize(n_missing = sum(is.na(pobreza_personas)),
             suma_pobreza_personas = sum(pobreza_personas, na.rm = TRUE))
 
-
+# crear codebook o libro de variables, usando los nombres de columna originales y los actuales
 names(pobreza)
-names(pobreza_malo_3b)
+names(pobreza_3b)
 
+# crear una tabla que una los dos vectores
 tibble(
   etiqueta = names(pobreza),
-  variable = names(pobreza_malo_3b)
+  variable = names(pobreza_3b)
 )
 
+# crear una tabla manualmente
 tribble(~etiqueta, ~variable,
-        "Años",        "anio",
-        "Edades", NA) |> 
-  filter(!is.na(variable))
+        "Años",    "anio",
+        "Edades",  "edad_cat"
+        )
 
-pobreza_malo_3b |> 
+# convertir varias variables a numérico
+pobreza_3b |> 
   mutate(pobreza_personas = as.numeric(pobreza_personas),
          pobreza_porcentaje = as.numeric(pobreza_porcentaje),
          limite_inferior = as.numeric(limite_inferior)
   )
 
-pobreza_malo_4 <- pobreza_malo_3b |> 
+# convertir varias variables a numérico con un solo uso de mutate()
+# usando el verbo across() (a través de)
+# al usar mutate(across()) aplicamos una misma operación a varias columnas al mismo tiempo
+pobreza_4 <- pobreza_3b |> 
   mutate(
     across(
       # columnas a transformar
@@ -135,7 +164,11 @@ pobreza_malo_4 <- pobreza_malo_3b |>
       # función a aplicar
       as.numeric))
 
-pobreza_malo_3b |> 
+pobreza_4
+
+# otro ejemplo: se especifican primero las columnas que vamos a afectar, 
+# y luego la función que les vamos a aplicar
+pobreza_3b |> 
   mutate(
     across(
       # columnas a transformar
@@ -147,7 +180,8 @@ pobreza_malo_3b |>
       ~as.numeric(.x))
   )
 
-pobreza_malo_3b |> 
+# también se pueden seleccionar las columnas usando números (posición de las columnas)
+pobreza_3b |> 
   mutate(
     across(
       # columnas a transformar
@@ -155,7 +189,7 @@ pobreza_malo_3b |>
       # función a aplicar
       as.numeric))
 
-pobreza_malo_3b |> 
+pobreza_3b |> 
   mutate(
     across(
       # columnas a transformar
@@ -163,23 +197,17 @@ pobreza_malo_3b |>
       # función a aplicar
       as.numeric))
 
-pobreza_malo_3b |> 
+# transformar todas las columnas que sean de un cierto tipo
+pobreza_3b |> 
   mutate(
     across(
       # columnas a transformar
-      where(is.character),
+      where(is.character), # todas las variables de texto
       # función a aplicar
       as.numeric))
 
-pobreza_malo_3b |> 
-  mutate(
-    across(
-      # columnas a transformar
-      c(where(is.character), -region, -nombre_comuna),
-      # función a aplicar
-      as.numeric))
-
-pobreza_malo_3b |> 
+# mezclar formas de selección: todas las de texto, menos dos específicas
+pobreza_3b |> 
   mutate(
     across(
       # columnas a transformar
@@ -187,8 +215,8 @@ pobreza_malo_3b |>
       # función a aplicar
       as.numeric))
 
-
-pobreza_malo_4 |> 
+# contar todos los datos perdidos de varias columnas
+pobreza_4 |> 
   summarise(
     across(
       # a estas columnas
@@ -198,7 +226,8 @@ pobreza_malo_4 |>
     )
   )
 
-pobreza_malo_4 |> 
+# calcular la suma de varias columnas, especificando el arugmento para saltarse los datos perdidos
+pobreza_4 |> 
   summarise(
     across(
       # a estas columnas
@@ -208,34 +237,51 @@ pobreza_malo_4 |>
     )
   )
 
+pobreza_4
 
-pobreza_malo_4 |> 
-  mutate(pobreza_personas = ifelse(is.na(pobreza_personas), 0, pobreza_personas))
+# aumentar los datos perdidos
+pobreza_4b <- pobreza_4 |> 
+  make_missing(cols = "pobreza_personas", messiness = 0.3)
 
-pobreza_malo_4 |> 
+pobreza_4b
+
+# rellenar datos perdidos con ceros
+pobreza_4b |> 
+  mutate(pobreza_personas = ifelse(is.na(pobreza_personas), 
+                                   0, 
+                                   pobreza_personas))
+
+# rellenar datos perdidos con un texto
+pobreza_4b |> 
+  mutate(pobreza_personas = ifelse(is.na(pobreza_personas), "(Sin datos)", pobreza_personas))
+
+# arreglar los decimales antes de rellenar datos perdidos
+pobreza_4b |> 
   mutate(pobreza_personas = round(pobreza_personas, 0),
          pobreza_personas = ifelse(is.na(pobreza_personas), "(Sin datos)", pobreza_personas))
 
+
+# instalar paquete que nos ayuda con la limpieza de los datos
 install.packages("tidyr")
 library(tidyr)
 
-
-pobreza_malo_4 |> 
+# reemplazar perdidos con la función replace_na()
+pobreza_4 |> 
   mutate(pobreza_personas = replace_na(pobreza_personas, 0))
 
-
-pobreza_malo_4 |> 
+# rellenar datos perdidos con el promedio
+pobreza_4 |> 
   mutate(pobreza_personas = ifelse(is.na(pobreza_personas), 
                                    mean(pobreza_personas, na.rm = T), 
                                    pobreza_personas))
 
-pobreza_malo_4 |> 
+pobreza_4 |> 
   group_by(region) |> 
   mutate(pobreza_personas = ifelse(is.na(pobreza_personas), 
                                    mean(pobreza_personas, na.rm = T), 
                                    pobreza_personas))
 
-pobreza_malo_4 |> 
+pobreza_4 |> 
   # select(region, nombre_comuna, pobreza_personas) |> 
   group_by(region) |> 
   mutate(pobreza_personas_2 = ifelse(is.na(pobreza_personas), 
@@ -243,65 +289,103 @@ pobreza_malo_4 |>
                                      pobreza_personas))
 
 
-pobreza_4 <- pobreza_malo_4
+pobreza_4 <- pobreza_4
 
 # ----
 
+# cargar un conjunto de datos distinto
+censo <- read_xlsx("datos/estimaciones-y-proyecciones-2002-2035-comunas.xlsx")
+
+# podemos cargarlo y realizar la limpieza inmediatamente
 censo <- read_xlsx("datos/estimaciones-y-proyecciones-2002-2035-comunas.xlsx") |> 
-  clean_names() |> 
+  janitor::clean_names() |> 
   rename(sexo = 7)
 
+censo
+
+# Intentemos encontrar un dato específico
 censo |> 
   filter(nombre_comuna == "La Florida") |> 
   select(poblacion_2010)
+# No resulta muy conveniente por la forma en que está construida esta tabla
 
 censo |> 
   glimpse()
+# en esa tabla, existe una variable, los años, que en vez de estar en una sola columna, están distribuidos en múltiples columnas
+# esto es típico en tablas hechas para ser leídas por personas, pero no son muy convenientes para realizar análisis sobre ellas
 
 library(tidyr)
 
+# pivotar las columnas a una sola columna que contenga los nombres de las columnas, y otra que contenga el valor de las columnas
 censo_2 <- censo |> 
   pivot_longer(cols = starts_with("poblacion"),
                names_to = "año",
                values_to = "poblacion")
 
+censo_2
+
+# Separar las columnas que contienen los años de su enunciado
 censo_3 <- censo_2 |> 
   separate(col = año, sep = "_", into = c("tipo", "año")) |> 
   mutate(año = as.numeric(año))
+# puede tardarse unos segundos
 
-
+# ahora sí podemos filtrar los datos como corresponde
 censo_3 |> 
   filter(nombre_comuna == "La Florida",
          año == 2025)
 
+censo_3 |> 
+  filter(nombre_comuna == "La Florida",
+         año == 2010)
+
+# calcular la suma de los datos por región y año
 censo_region <- censo_3 |> 
   group_by(nombre_region, año) |> 
   summarise(poblacion = sum(poblacion))
+
+censo_region
 
 censo_3 |> 
   group_by(nombre_region, nombre_comuna, año) |> 
   summarise(poblacion = sum(poblacion)) |> 
   print(n=100)
 
-options(scipen = 9999)
-
+# pivotar a ancho (la operación inversa a la que hicimos recién), dónde los valores de una columna se usan como los nombres de las nuevas columnas,
+# específica de qué otra columna sacamos los valores que van a contenerse en estas columnas nuevas
 censo_region_2 <- censo_region |> 
   pivot_wider(names_from = año,
               values_from = poblacion)
 
+censo_region
+
+# guaradr el resultado como excel
 censo_region_2 |> 
   writexl::write_xlsx("datos/estimaciones-regiones.xlsx")
 
+# —----
+# leer otro conjunto de datos del censo
+read_xlsx("datos/P5_Genero.xlsx")
+#  este archivo viene en múltiples ho
 
+# especificar la hoja que vamos a leer
 genero <- read_xlsx("datos/P5_Genero.xlsx", sheet = 2)
+
+# también puede ser por el nombre exacto de la hoja
 genero <- read_xlsx("datos/P5_Genero.xlsx", sheet = "1")
 
+genero
+
+library(janitor)
+
+# limpieza
 genero |> 
   row_to_names(3) |> 
   clean_names() |> 
   mutate(across(3:10, as.numeric)) |> 
   filter(!is.na(region))
 
+# limpiar y pivotar
 genero_2 <- genero |> 
   row_to_names(3) |> 
   # clean_names() |> 
@@ -312,9 +396,12 @@ genero_2 <- genero |>
 
 genero_2
 
+# —----
 
+# cargar otro conjunto
 eleccion <- readr::read_csv2("datos/presidenciales_2021_comuna.csv")
 
+# explorarlo
 eleccion |> 
   count(eleccion)
 
@@ -355,44 +442,73 @@ eleccion_region <- eleccion_2 |>
   summarise(votos = sum(votos)) |> 
   filter(candidatura %in% c("Gabriel Boric", "Jose Antonio Kast"))
 
+eleccion_region
+
 eleccion_region_2 <- eleccion_region |> 
   pivot_wider(names_from = candidatura, values_from = votos)
 
 eleccion_region_2
 
-
+# pivotar a ancho
 eleccion_3 <- eleccion_2 |> 
   filter(candidatura %in% c("Gabriel Boric", "Jose Antonio Kast")) |> 
   select(2:7) |> 
   pivot_wider(names_from = candidatura, values_from = votos)
 
+eleccion_3
+
+# —----
+
+# unir conjuntos de datos
+
+# quedamos con dos conjuntos de datos que son coincidentes:
+# ambos conjuntos tienen una observación por comuna
 
 eleccion_3
 pobreza_4
 
+# podemos unir ambas tablas para que las filas que sean de una columna X coincidan con las filas de la otra tabla de la misma comuna
+
+# unir por la variable comuna
 left_join(eleccion_3,
           pobreza_4, 
           by = "comuna")
+# no se puede! en la segunda tabla (tabla Y) no hay una variable que se llame comuna
 
+# especificar que la unión es por columnas que se llaman distinto
 left_join(eleccion_3,
           pobreza_4, 
           by = c("comuna" = "nombre_comuna"))
+# El resultado es inexacto, porque quedan varias comunas sin datos
+# esto significa la coincidencia entre columnas no es perfecta, porque las comunas tienen nombres levemente distintos en ambas tablas
 
+# Por suerte tenemos el código único territorial de las comunas, que permite una unión de los datos con menos errores
 left_join(eleccion_3,
           pobreza_4, 
           by = c("cut_comuna" = "codigo"))
+# pero no permite ser la unión porque son de distinto tipo!
 
+# entonces nos dedicamos a preparar los datos para hacer una unión que funcione mejor
+
+# primero renombramos las columnas de unión para que se llamen igual
 eleccion_4 <- eleccion_3 |> 
   rename(codigo_comuna = cut_comuna)
 
+# Luego convertimos la columna de unión a un valor común, y seleccionamos 
+# solamente las columnas nuevas que queremos agregarle a la primera tabla
 pobreza_5 <- pobreza_4 |> 
   rename(codigo_comuna = codigo) |> 
   mutate(codigo_comuna = as.numeric(codigo_comuna)) |> 
   select(codigo_comuna, poblacion, pobreza_porcentaje)
 
+# Ahora que tenemos datos más preparados y limpios para la unión, esta funciona de inmediato
 eleccion_pobreza <- left_join(eleccion_4,
                               pobreza_5, 
                               by = "codigo_comuna")
+
+eleccion_pobreza
+
+eleccion_pobreza |> filter(is.na(pobreza_porcentaje))
 
 
 eleccion_pobreza |> 
