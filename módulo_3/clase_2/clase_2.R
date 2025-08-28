2dc053116874d5763a6615a6566aeb037b09aecd
bolea <bastian.olea@subdere.gov.cl>
2025-08-28 13:47:56 -0400

    clases finales

--- módulo_3/clase_2/clase_2.R
+++ módulo_3/clase_2/clase_2.R
@@ -1,142 +1,7 @@
-install.packages("ggplot2")
-
 library(ggplot2)
-
-ggplot()
-
-iris
-mtcars
-
-# ggplot ----
-ggplot(iris)
-
-iris |> 
-  ggplot()
-
-names(iris)
-
-## dispersión ----
-iris |> 
-  ggplot() +
-  # mapeo
-  aes(x = Sepal.Length, y = Sepal.Width) +
-  # geometría
-  geom_point()
-
-
-iris |> 
-  ggplot() +
-  # mapeo
-  aes(x = Sepal.Length, y = Sepal.Width, colour = Species) +
-  # geometría
-  geom_point()
-
-iris |> 
-  ggplot() +
-  # mapeo
-  aes(x = Petal.Width, y = Petal.Length, colour = Species) +
-  # geometría
-  geom_point()
-
-
-iris |> 
-  ggplot() +
-  # mapeo
-  aes(x = Petal.Width, 
-      y = Sepal.Width, 
-      colour = Petal.Length) +
-  # geometría
-  geom_point()
-
-
-
-iris |> 
-  ggplot() +
-  # mapeo
-  aes(x = Petal.Width, y = Petal.Length, colour = Species) +
-  # geometría
-  geom_point(colour = "orchid", size = 5, alpha = 0.3)
-
-iris |> 
-  ggplot() +
-  # mapeo
-  aes(x = Petal.Width, y = Petal.Length, alpha = Sepal.Length) +
-  # geometría
-  geom_point(colour = "orchid", size = 5)
-
-iris |> 
-  ggplot() +
-  # mapeo
-  aes(x = Petal.Width, y = Petal.Length, size = Petal.Width) +
-  # geometría
-  geom_point(colour = "orchid", alpha = 0.3)
-
-iris |> 
-  ggplot() +
-  # mapeo
-  aes(x = Petal.Width, y = Petal.Length, color = Petal.Width) +
-  # geometría
-  geom_point(alpha = 0.3)
-
-## histograma ----
-iris |> 
-  ggplot() +
-  aes(x = Petal.Length) +
-  geom_histogram()
-
-iris |> 
-  ggplot() +
-  aes(x = Sepal.Length) +
-  geom_histogram(bins = 10)
-
-iris |> 
-  ggplot() +
-  aes(x = Sepal.Length) +
-  geom_histogram(binwidth = 0.2)
-
-iris |> 
-  ggplot() +
-  aes(x = Sepal.Length) +
-  geom_histogram(binwidth = 0.2, colour = "palegreen4", fill = "palegreen3")
-
-
-iris |> 
-  ggplot() +
-  aes(x = Sepal.Length) +
-  geom_density()
-
-iris |> 
-  ggplot() +
-  aes(x = Sepal.Width) +
-  geom_density(fill = "tomato", colour = "tomato3", alpha = 0.6)
-
-
-iris |> 
-  ggplot() +
-  aes(x = Sepal.Width, fill = Species) +
-  geom_density(alpha = 0.4, linewidth = 0)
-
-iris |> 
-  ggplot() +
-  aes(x = Sepal.Width, 
-      fill = Species, color = Species) +
-  geom_density(alpha = 0.4)
-
-
-iris |> 
-  ggplot() +
-  # mapeo
-  aes(x = Petal.Width, y = Petal.Length, color = Petal.Width) +
-  # geometría
-  geom_point(alpha = 0.3) +
-  scale_x_continuous(name = "Ancho", breaks = c(0, 1, 2, 3))
-  # scale_x_continuous(name = "Ancho", n.breaks = 10)
-
-
-
-# datos ----
 library(dplyr)
 
+# seguimos explorando el dataset de temperaturas
 temp <- readr::read_csv2("datos/temperaturas_chile.csv")
 
 temp |> 
@@ -144,160 +9,9 @@ temp |>
 
 temp |> distinct(nombre) |> print(n=Inf)
 
-temp |> 
-  select(-t_med) |> 
-  na.omit()
-
-temp |> 
-  filter(nombre == "Quinta Normal, Santiago") |> 
-  ggplot() +
-  aes(t_max) +
-  geom_histogram()
-
-temp |> 
-  # filter(nombre == "El Loa, Calama Ad.") |>
-  filter(nombre == "Quinta Normal, Santiago") |>
-  ggplot() +
-  aes(t_max) +
-  geom_histogram()
-
-temp |> 
-  # filter(nombre == "El Loa, Calama Ad.") |>
-  filter(nombre == "Quinta Normal, Santiago") |>
-  ggplot() +
-  aes(fecha) +
-  geom_histogram()
-
-temp |> 
-  filter(nombre == "Quinta Normal, Santiago") |>
-  ggplot() +
-  aes(t_max) +
-  geom_histogram() +
-  geom_vline(xintercept = 20)
-
-temp |> 
-  filter(nombre == "Quinta Normal, Santiago") |>
-  ggplot() +
-  geom_histogram(aes(t_max)) +
-  geom_vline(aes(xintercept = mean(t_max, na.rm = T)))
-
-
-temp_q <- temp |> 
-  filter(nombre == "Quinta Normal, Santiago") |>
-  # filter(nombre == "El Loa, Calama Ad.") |>
-  filter(!is.na(t_max))
-
-prom <- mean(temp_q$t_max)
-
-temp_q |> 
-  ggplot() +
-  aes(t_max) +
-  geom_histogram() +
-  geom_vline(xintercept = prom, 
-             colour = "indianred3", linewidth = 1.2)
-
-
-temp_q |> 
-  mutate(tipo = ifelse(t_max > prom, 
-                       "alta", "baja")) |> 
-  ggplot() +
-  aes(t_max, fill = tipo) +
-  geom_histogram() +
-  geom_vline(xintercept = prom, 
-             colour = "indianred3", linewidth = 1.2)
-
-temp_q |> 
-  ggplot() +
-  aes(t_max, fill = t_max > prom) +
-  geom_histogram() +
-  geom_vline(xintercept = prom, 
-             colour = "indianred3", linewidth = 1.2)
-
-temp_q |> 
-  mutate(tipo = ifelse(t_max > prom, 
-                       "alta", "baja")) |> 
-  ggplot() +
-  aes(t_max, fill = tipo) +
-  geom_histogram() +
-  geom_vline(xintercept = prom, 
-             colour = "cyan3", linewidth = 1.2) +
-  scale_fill_manual(values = c("alta" = "brown2", 
-                               "baja" = "royalblue2")) +
-  annotate(geom = "text", x = prom*0.96, y = 250, 
-           label = round(prom, 1), hjust = 1) +
-  theme_classic()
-  
-
-
-temp |> 
-  filter(nombre == "Quinta Normal, Santiago",
-         año > 2018) |> 
-  ggplot() +
-  aes(t_min, t_max, colour = mes) +
-  geom_point(alpha = 0.3)
-
-# {gganimate}
-
-temp |> 
-  filter(nombre == "Quinta Normal, Santiago",
-         año > 2018) |> 
-  ggplot() +
-  aes(x = fecha, y = t_max) +
-  geom_line(linewidth = 0.1)
-
-
-temp |> 
-  filter(nombre == "Quinta Normal, Santiago",
-         año > 2018) |> 
-  ggplot() +
-  aes(x = fecha, y = t_max, color = t_max) +
-  geom_line(linewidth = 0.1) +
-  # scale_color_gradient(low = "royalblue2", high = "brown2")
-  scale_color_gradient2(low = "royalblue2", mid = "black", 
-                        high = "brown2", midpoint = 22)
-
-
-temp_q <- temp |> 
-  filter(nombre == "Quinta Normal, Santiago",
-         año > 2018)
-
-prom <- mean(temp_q$t_max, na.rm = TRUE)
-prom <- temp_q |> 
-  summarise(mean(t_max, na.rm = TRUE)) |> 
-  pull()
-
-temp_q |> 
-  ggplot() +
-  aes(x = fecha, y = t_max, color = t_max) +
-  geom_line(linewidth = 0.1) +
-  scale_color_gradient(low = "royalblue2", high = "brown2") +
-  geom_hline(yintercept = prom)
-
-temp_q |> 
-  ggplot() +
-  aes(x = fecha, y = t_max, color = t_max) +
-  geom_line(linewidth = 0.1) +
-  scale_color_gradient(low = "royalblue2", high = "brown2") +
-  geom_hline(yintercept = prom)
-
-
-
-temp |> distinct(nombre)
-
-temp |> 
-  filter(nombre == "Eulogio Sánchez, Tobalaba Ad.",
-         año > 2018) |> 
-  ggplot() +
-  aes(fecha, t_max) +
-  geom_point(alpha = 0.3) +
-  geom_smooth(method = "lm")
-
-
-
-# —----
-
 estacion <- "Quinta Normal, Santiago"
 
+# crear gráfico y asignarlo a un objeto
 grafico <- temp |> 
   filter(nombre == estacion,
          año > 2018) |> 
@@ -306,6 +20,11 @@ grafico <- temp |>
   geom_line(linewidth = 0.1) +
   scale_color_gradient(low = "royalblue2", high = "brown2")
 
+# ver el gráfico
+grafico
+
+## temas ----
+# agregarle más capas
 grafico +
   theme_minimal()
 
@@ -315,6 +34,7 @@ grafico +
 grafico +
   theme_linedraw()
 
+# personalizar apariencia especificando el tema
 grafico +
   scale_y_continuous(minor_breaks = c(15, 25, 35)) +
   theme_classic() +
@@ -322,7 +42,6 @@ grafico +
                                    colour = "black"),
         axis.text.x = element_text(face = "italic", 
                                    colour = "grey")) +
-  # theme(panel.background = element_rect(fill = "grey95")) +
   theme(panel.grid.major = element_line(colour = "gray90", 
                                         linewidth = 0.3),
         panel.grid.minor = element_line(colour = "gray90", 
@@ -331,6 +50,7 @@ grafico +
 grafico +
   theme_minimal()
 
+# agregar textos
 grafico +
   theme_minimal() +
   labs(title = "Fluctuación de temperaturas",
@@ -341,29 +61,32 @@ grafico +
   theme(plot.title = element_text(family = "Verdana", face = "bold"))
 
 
-
+## gráficos de barras ----
+# filtrar los datos
 estacion <- "Quinta Normal, Santiago"
 
 temp_filt <- temp |> 
   filter(nombre == estacion,
          año > 2018)
 
-
+# gráfico de barras que genera un conteo de las observaciones de la variable elegida
 temp |> 
   filter(!is.na(zona_geografica)) |> 
   ggplot() +
   aes(zona_geografica) +
   geom_bar()
 
-
+# contar primero y luego crear barras en base a la variable y su conteo, por separado
 temp |> 
   filter(!is.na(zona_geografica)) |> 
   distinct(zona_geografica, nombre) |> 
+  # conteo
   count(zona_geografica) |> 
   ggplot() +
-  aes(zona_geografica, n) +
+  aes(x = zona_geografica, y = n) + # el eje y tiene el alto de cada barra
   geom_col(width = .6)
 
+# si damos vuelta x e y, las barras aparecen laterales
 temp |> 
   filter(!is.na(zona_geografica)) |> 
   distinct(zona_geografica, nombre) |> 
@@ -372,39 +95,45 @@ temp |>
   aes(n, zona_geografica) +
   geom_col(width = .6)
 
-temp |> 
-  filter(!is.na(zona_geografica)) |> 
-  distinct(zona_geografica, nombre) |> 
-  count(zona_geografica) |> 
-  ggplot() +
-  aes(x = zona_geografica, y = n) +
-  geom_col(width = .6) +
-  geom_text(aes(label = n, y = n+0.6)) +
-  scale_y_continuous(expand = expansion(c(0, 0.1)))
-
-
+# agregar conteos encima de las barras
 temp_conteo <- temp |> 
   filter(!is.na(zona_geografica)) |> 
   distinct(zona_geografica, nombre) |> 
   count(zona_geografica) |> 
   arrange(n)
 
+temp_conteo
 
+temp_conteo |> 
+  ggplot() +
+  aes(x = zona_geografica, y = n) +
+  geom_col(width = .6) +
+  geom_text(aes(label = n, y = n+0.6)) +
+  scale_y_continuous(expand = expansion(c(0, 0.1)))
+
+## ordenar las barras ----
 class(temp_conteo$zona_geografica)
 
+# las variables de texto tienen un orden alfabético
 variable <- c("alto", "medio", "bajo")
 
 sort(variable)
 
+# necesitamos establecer un orden específico para la variable
+# para esto existe el tipo de datos "factor"
 variable_factor <- factor(variable, c("alto", "medio", "bajo"))
 
 variable_factor |> sort()
+# ahora los datos aparecen en el orden esperado
 
+# paquete para trabajar con factores
 library(forcats)
 # install.packages("forcats")
 
+# ver nombres de los niveles de la variable
 temp_conteo$zona_geografica |> cat(sep = "\n")
 
+# ordenar los niveles del factor manualmente
 temp_conteo |> 
   mutate(zona_geografica = fct_relevel(zona_geografica,
                                        "Litoral",
@@ -417,7 +146,7 @@ temp_conteo |>
   geom_text(aes(label = n, y = n+0.6)) +
   scale_y_continuous(expand = expansion(c(0, 0.1)))
 
-
+# ordenar los niveles del factor por el orden de una segunda variable (con los conteos)
 temp_conteo |> 
   mutate(zona_geografica = fct_reorder(zona_geografica,
                                        n, .desc = T)) |> 
@@ -438,17 +167,22 @@ temp |>
   aes(año, t_max) +
   geom_col()
 
+# sacar 3 estaciones al azar
 estaciones <- unique(temp$nombre)
 
 estacion <- sample(estaciones, 3)
 
 estacion
 
-temp |> 
-  filter(nombre %in% estacion) |> 
+temp_azar <- temp |> 
+  filter(nombre %in% estacion)
+
+temp_azar_año <- temp_azar |> 
   filter(año >= 2010) |> 
   group_by(año, nombre) |> 
-  summarise(t_max = max(t_max, na.rm = T)) |> 
+  summarise(t_max = max(t_max, na.rm = T))
+
+temp_azar_año |> 
   ggplot() +
   aes(año, t_max, fill = t_max) +
   geom_col(width = 0.5) +
@@ -466,9 +200,10 @@ temp |>
   theme(legend.key.width = unit(2, "mm")) +
   labs(subtitle = "Temperaturas máximas")
 
-# guardar
+# guardar gráfico
 ggsave("temperaturas.jpg")
 
+# guardar con un tamaño distinto al por defecto
 ggsave("temperaturas_b.jpg", 
        width = 9, height = 6,
        scale = 1.2)
--- módulo_3/clase_3/clase_3.R
+++ módulo_3/clase_3/clase_3.R
@@ -1,58 +1,57 @@
 library(dplyr)
 library(ggplot2)
 
+# cargar datos de temperatura
 temp <- readr::read_csv2("datos/temperaturas_chile.csv")
 
 temp
 
+## histograma ----
 temp |> 
   ggplot() +
   aes(t_max) +
   geom_histogram()
 
+## densidad ----
 temp |> 
   ggplot() +
   aes(t_max) +
   geom_density()
 
+## boxplot ----
 temp |> 
   ggplot() +
   aes(x = t_max) +
   geom_boxplot()
-
-temp |> 
-  ggplot() +
-  aes(y = t_max) +
-  geom_boxplot()
+# muestra la distribución de los datos con una caja que representa los percentiles 25, 50 (mediana) y 75
 
 temp |> 
   ggplot() +
   aes(y = t_max, color = zona_geografica) +
   geom_boxplot()
 
-
+## violín ----
+# muestra la distribución de los datos
 temp |> 
   ggplot() +
   aes(1, y = t_max) +
   geom_violin()
+# en este caso ponemos la distribución sobre el número 1, para que sea una sola figura
 
-temp |> 
-  ggplot() +
-  aes(1, y = t_max) +
-  geom_violin()
-
+# una distribución por cada valor de la variable
 temp |> 
   ggplot() +
   aes(x = zona_geografica, y = t_max) +
   geom_violin()
 
-
+# combinar capas de geometrías
 temp |> 
   ggplot() +
   aes(x = zona_geografica, y = t_max) +
   geom_violin() +
   geom_boxplot()
 
+# modificar apariencia
 temp |> 
   ggplot() +
   aes(x = zona_geografica, y = t_max) +
@@ -68,7 +67,8 @@ temp |>
   geom_boxplot(alpha = 0.6, outlier.size = 1, outliers = F) +
   theme_classic()
 
-
+## puntos repartidos ----
+# permite que los puntos se muevan al azar en una dirección para poder ver distribuciones cuando hay demasiados puntos en una misma posición
 temp |> 
   ggplot() +
   aes(x = zona_geografica, y = t_max, 
@@ -78,11 +78,14 @@ temp |>
   geom_jitter(alpha = 0.1, size = 0.2) +
   theme_classic()
 
-
-temp |> 
+# filtrar los datos
+# filtrar datos
+temp_f <- temp |> 
   filter(!is.na(zona_geografica),
          año == 2023,
-         mes == 4) |> 
+         mes == 4)
+
+temp_f |> 
   ggplot() +
   aes(x = zona_geografica, y = t_max, 
       colour = zona_geografica, fill = zona_geografica) +
@@ -91,34 +94,7 @@ temp |>
               height = 0, width = 0.2) +
   theme_classic()
 
-
-temp |> 
-  filter(!is.na(zona_geografica),
-         año == 2023,
-         mes == 4) |> 
-  ggplot() +
-  aes(x = zona_geografica, y = t_max, 
-      colour = zona_geografica, fill = zona_geografica) +
-  geom_violin(alpha = 0.4) +
-  geom_jitter(alpha = 0.4, size = 0.4,
-              height = 0, width = 0.2) +
-  theme_classic()
-
-temp_f <- temp |> 
-  filter(!is.na(zona_geografica),
-         año == 2023,
-         mes == 4)
-
-temp_r <- temp_f |> 
-  group_by(zona_geografica) |> 
-  summarise(promedio = mean(t_max, na.rm = T),
-            median = median(t_max, na.rm = T))
-
-temp_r |> 
-  ggplot() +
-  aes(zona_geografica, promedio) +
-  geom_point()
-
+# combinar jitter con violín
 temp_f |> 
   ggplot() +
   aes(x = zona_geografica, y = t_max, 
@@ -126,37 +102,61 @@ temp_f |>
   geom_violin(alpha = 0.4) +
   geom_jitter(alpha = 0.4, size = 0.4,
               height = 0, width = 0.2) +
+  theme_classic()
+
+
+# calcular resumen de datos
+temp_r <- temp_f |> 
+  group_by(zona_geografica) |> 
+  summarise(promedio = mean(t_max, na.rm = T),
+            median = median(t_max, na.rm = T))
+
+# prueba de cómo queda el resumen
+temp_r |> 
+  ggplot() +
+  aes(zona_geografica, promedio) +
+  geom_point()
+
+# poner el resumen encima del gráfico
+# para esto, creamos una capa que usa sus propios datos
+temp_f |> 
+  ggplot() +
+  aes(x = zona_geografica, y = t_max, 
+      colour = zona_geografica, fill = zona_geografica) +
+  geom_violin(alpha = 0.4) +
+  geom_jitter(alpha = 0.4, size = 0.4,
+              height = 0, width = 0.2) +
+  # capa que usa datos de otro objeto distinto al principal del gráfico
   geom_point(data = temp_r,
              aes(zona_geografica, promedio),
              size = 3, color = "black") +
   theme_classic()
 
 
-temp |> 
+## mosaico ----
+# calcular resumen de datos por año y mes
+temp_m <- temp |> 
   filter(año > 2015) |> 
   group_by(año, mes) |> 
   summarize(t_max = mean(t_max, na.rm = T),
-            n = n()) |> 
+            n = n())
+
+# visualizar con puntos para obtener una suerte de matriz
+temp_m |> 
   ggplot() +
   aes(x = año, y = mes, color = n) +
   geom_point()
 
-
-temp |> 
+# visualizar con geometría de baldozas o mapa de calor
+temp_m |> 
   filter(año > 2015) |> 
-  group_by(año, mes) |> 
-  summarize(t_max = mean(t_max, na.rm = T),
-            n = n()) |> 
   ggplot() +
   aes(x = año, y = mes, fill = t_max) +
   geom_tile()
 
-
-temp |> 
+# aplicar escala de colores viridis
+temp_m |> 
   filter(año > 1980) |>
-  group_by(año, mes) |> 
-  summarize(t_max = mean(t_max, na.rm = T),
-            n = n()) |> 
   ggplot() +
   aes(x = año, y = mes, fill = t_max) +
   geom_tile() +
@@ -164,11 +164,8 @@ temp |>
   scale_fill_viridis_c(option = "inferno")
 
 
-temp |> 
+temp_m |> 
   filter(año > 1990) |>
-  group_by(año, mes) |> 
-  summarize(t_max = mean(t_max, na.rm = T),
-            n = n()) |> 
   ggplot() +
   aes(x = año, y = mes, fill = t_max) +
   geom_tile() +
@@ -179,52 +176,58 @@ temp |>
   theme(axis.text.x = element_text(angle = -90,
         hjust = 0, vjust = 0.5))
 
-
-
-
-temp |> 
+# dispersión con texto ----
+grafico_disp <- temp |> 
   filter(año == 2023,
          mes == 3,
          dia == 15) |> 
   ggplot() +
   aes(t_min, t_max) +
   geom_point(size = 3, alpha = 0.5) +
-  theme_linedraw() +
-  # geom_text(aes(label = t_max), size = 3)
-  # geom_text(aes(label = t_max, y = t_max+1), size = 3)
-  ggrepel::geom_text_repel(aes(label = t_max), size = 3)
+  theme_linedraw()
 
+grafico_disp
 
-temp |> 
-  filter(año == 2023,
-         mes == 3,
-         dia == 15) |> 
-  ggplot() +
-  aes(t_min, t_max) +
-  geom_point(size = 3, alpha = 0.5) +
-  theme_linedraw() +
+# agregar texto
+grafico_disp + geom_text(aes(label = t_max), size = 3)
+# no queda bien
+  
+# probar mover los puntos manualmente
+grafico_disp + geom_text(aes(label = t_max, y = t_max+1), size = 3)
+# queda mejor, pero no es ideal
+
+# mover puntos automáticamente con {ggrepel}
+# install.packages("ggrepel")
+grafico_disp + ggrepel::geom_text_repel(aes(label = t_max), size = 3)
+
+# filtrar los datos que pasan a una capa
+grafico_disp +
   ggrepel::geom_text_repel(data = ~filter(.x, 
                                           t_max < 20,
                                           t_min < 5),
                              aes(label = t_max), size = 3)
 
-
-
 # —----
 
 library(arrow)
 
+# cargar datos de delincuencia
 cead <- arrow::read_parquet("datos/cead_delincuencia_chile.parquet")
 
+# sumar cantidad total de delitos
 cead_suma <- cead |> 
+  # filtrar el año
   filter(fecha >= "2013-01-01",
          fecha <= "2013-12-31") |> 
   group_by(delito) |> 
   summarize(n = sum(delito_n))
 
+# paquete para trabajar con fechas
 library(lubridate)
 
+# repetir pero con {lubridate}
 cead_suma <- cead |> 
+  # filtrar el año
   filter(year(fecha) == 2024) |> 
   group_by(delito) |> 
   summarize(n = sum(delito_n))
@@ -232,18 +235,20 @@ cead_suma <- cead |>
 cead_suma |> 
   arrange(desc(n))
 
-
+## barras ----
 cead_suma |> 
   ggplot() +
   aes(delito, n) +
   geom_col()
 
+# barras ordenadas
 cead_suma |> 
   mutate(delito = forcats::fct_reorder(delito, n)) |> 
   ggplot() +
   aes(n, delito) +
   geom_col()
 
+# barras ordenadas con filtro previo
 cead_suma |> 
   mutate(delito = forcats::fct_reorder(delito, n)) |> 
   filter(n > 10000) |> 
@@ -251,7 +256,7 @@ cead_suma |>
   aes(n, delito) +
   geom_col()
 
-
+# calcular mayores delitos
 top_delitos <- cead_suma |> 
   arrange(desc(n)) |> 
   slice_max(n, n = 10) |> 
@@ -259,6 +264,7 @@ top_delitos <- cead_suma |>
 
 top_delitos
 
+# filtrar para tener solo estos delitos
 cead_suma |> 
   filter(delito %in% top_delitos) |> 
   mutate(delito = forcats::fct_reorder(delito, n)) |> 
@@ -269,66 +275,91 @@ cead_suma |>
   # theme(legend.position = "none")
   guides(fill = guide_none())
 
-
-cead_suma |> 
+## tortas ----
+# para hacer un gráfico de torta, primero hay que hacer una barra apilada
+grafico_cead <- cead_suma |> 
   filter(delito %in% top_delitos) |> 
   mutate(delito = forcats::fct_reorder(delito, n)) |> 
   filter(n > 10000) |> 
   ggplot() +
-  aes(1, n, fill = delito) +
+  aes(1, n, fill = delito)
+
+grafico_cead + 
   geom_col()
 
-
-cead_suma |> 
-  filter(delito %in% top_delitos) |> 
-  mutate(delito = forcats::fct_reorder(delito, n)) |> 
-  filter(n > 10000) |> 
-  ggplot() +
-  aes(1, n, fill = delito) +
+# luego "enrollar" la barra cambiando la capa de coordenadas
+grafico_cead +
   geom_col(color = "white", linewidth = 1.2) +
   coord_polar(theta = "y") +
   theme_void()
 
-
-cead_suma |> 
-  filter(delito %in% top_delitos) |> 
-  mutate(delito = forcats::fct_reorder(delito, n)) |> 
-  filter(n > 10000) |> 
-  ggplot() +
-  aes(1, n, fill = delito) +
+# agregar texto
+grafico_cead +
   geom_col(color = "white", linewidth = 0.6) +
   coord_polar(theta = "y") +
   theme_void() +
   geom_text(aes(label = n))
+# sale pésimo, veamos por qué
 
+# los textos están apareciendo abajo, porque no están ajustándose a la barra apilada
+grafico_cead +
+  geom_col(color = "white", linewidth = 0.6) +
+  theme_void() +
+  geom_text(aes(label = scales::comma(n, big.mark = ".")),
+            size = 3)
 
-cead_suma |> 
-  filter(delito %in% top_delitos) |> 
-  mutate(delito = forcats::fct_reorder(delito, n)) |> 
-  filter(n > 10000) |> 
-  ggplot() +
-  aes(1, n, fill = delito) +
+# ajustar a la barra apilada
+grafico_cead +
+  geom_col(color = "white", linewidth = 0.6) +
+  theme_void() +
+  geom_text(aes(label = scales::comma(n, big.mark = ".")),
+            position = position_stack(0.5),
+            size = 3)
+
+# ahora volver a la torta
+grafico_cead +
+  geom_col(color = "white", linewidth = 0.6) +
+  theme_void() +
+  geom_text(aes(label = scales::comma(n, big.mark = ".")),
+            position = position_stack(0.5),
+            size = 3) +
+  coord_polar(theta = "y")
+
+# modificar la posición del texto para que aparezca más hacia un lado
+grafico_cead +
   geom_col(color = "white", linewidth = 0.6) +
   theme_void() +
   geom_text(aes(x = 1.3, label = scales::comma(n, big.mark = ".")),
             position = position_stack(0.5),
-            size = 3
-            ) +
+            size = 3)
+
+# aplicar a la torta
+grafico_cead +
+  geom_col(color = "white", linewidth = 0.6) +
+  theme_void() +
+  geom_text(aes(x = 1.3, label = scales::comma(n, big.mark = ".")),
+            position = position_stack(0.5),
+            size = 3) +
   coord_polar(theta = "y")
 
 
+## sumar valores en "otros" ----
 cead_suma |> 
   arrange(desc(n)) |> 
   print(n=20)
 
+# crear una variable que agrupa las categorías minoritarias en una sola
 cead_suma_otros <- cead_suma |> 
   arrange(desc(n)) |> 
-  mutate(delito2 = forcats::fct_lump_n(delito, n = 6, 
-                                       w = n, other_level = "Otros delitos")) |> 
+  mutate(delito2 = forcats::fct_lump_n(delito, 
+                                       n = 6, 
+                                       w = n, # variable con el conteo
+                                       other_level = "Otros delitos")) |>
+  # volver a sumar los grupos para colapsar el "otros"
   group_by(delito2) |> 
   summarize(n = sum(n))
 
-  
+# aplicar nueva fuente de datos al gráfico anterior
 cead_suma_otros |> 
   rename(delito = delito2) |> 
   # filter(delito %in% top_delitos) |> 
@@ -346,7 +377,8 @@ cead_suma_otros |>
                              ncol = 2, title = NULL))
 
 
-
+## donas ----
+# agregar expansión en el eje x para crear un espacio dentro de la torta
 cead_suma_otros |> 
   rename(delito = delito2) |> 
   # filter(delito %in% top_delitos) |> 
@@ -364,13 +396,14 @@ cead_suma_otros |>
   guides(fill = guide_legend(reverse = T, position = "bottom", 
                              ncol = 2, title = NULL))
 
-
+## lollypop ---- 
 cead_suma |> 
   mutate(delito = forcats::fct_reorder(delito, n)) |> 
   ggplot() +
   aes(n, delito) +
   geom_col()
 
+# en lugar de barras, ponemos puntos conectados con un segmento al origen del eje
 cead_suma |> 
   mutate(delito = forcats::fct_reorder(delito, n)) |> 
   filter(n > 50000) |> 
@@ -382,6 +415,8 @@ cead_suma |>
   geom_segment(aes(xend = 0, yend = delito))
 
 
+## temas ----
+# crear un gráfico base
 grafico <- cead_suma |> 
   mutate(delito = forcats::fct_reorder(delito, n)) |> 
   filter(n > 30000) |> 
@@ -391,25 +426,39 @@ grafico <- cead_suma |>
 
 grafico
 
+# cargar el paquete para los temas
 library(thematic)
 # install.packages("thematic")
 
+# definir dos colores
 thematic_on(bg = "gray20",
             fg = "indianred2")
 
+# aplicarlos al tema
 grafico
 
+# cambiar paleta
 thematic_on(bg = "#1F284D",
             fg = "#37C0C7")
 
+# ver como queda
+grafico
 
+
+## tipografías ----
 library(showtext)
 # install.packages("showtext")
 
+# agregar una tipografía desde Google Fonts
 font_add_google("Fira Code")
+
+# activar uso de tipografías
 showtext_auto()
+
+# ajustar tamaño de las letras
 showtext_opts(dpi = 250)
 
+# probar
 grafico +
   theme(text = element_text(family = "Fira Code")) +
   geom_text(aes(label = scales::comma(n, big.mark = "."),
@@ -419,4 +468,5 @@ grafico +
        subtitle = "Año 2024") +
   theme(plot.title.position = "plot")
 
+# desactivar temas
 thematic_off()
